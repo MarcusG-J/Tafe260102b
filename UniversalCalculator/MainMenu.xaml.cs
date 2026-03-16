9e34daad4068c3c14021ccb2015885a9a6bfbae2
Marcus Griffith-Jones <marcus.griffith-jones@tafesa.edu.au>
2026-03-16 13:22:03 +1030

    Added main menu and images

---
+++ UniversalCalculator/MainMenu.xaml.cs
@@ -0,0 +1,35 @@
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Calculator
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainMenu : Page
+	{
+		public MainMenu()
+		{
+			InitializeComponent();
+		}
+
+		private void MathCalcButton_Click(object sender, RoutedEventArgs e)
+		{
+			this.Frame.Navigate(typeof(MainPage));
+		}
+
+		private void ExitButton_Click(object sender, RoutedEventArgs e)
+		{
+			CoreApplication.Exit();
+
+
+		}
+	}
+}
